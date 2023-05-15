a097a6402fa1aba000fcb8d2061d5882a7047322
GeraldFishta <ge.fishta@gmail.com>
2023-05-15 16:41:48 +0200

    aggiunta right aside

--- Homepage.js
+++ Homepage.js
@@ -1,4 +1,4 @@
-fetch("https://striveschool-api.herokuapp.com/api/deezer/",{
+fetch("https://striveschool-api.herokuapp.com/api/deezer/album/75621062",{
     headers: {
     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDYyM2M3ODc3NTgzYzAwMTRkMmNjYmMiLCJpYXQiOjE2ODQxNTk2MDksImV4cCI6MTY4NTM2OTIwOX0.RK6RE8424MDCQbvs8u0gvKiPo4MrR6-ww1HYYA6TexE"
     }})
