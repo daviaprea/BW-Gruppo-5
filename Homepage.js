e0a92f9c5cb5136edf68f9a90383989306ebc418
daviaprea <apredav@gmail.com>
2023-05-15 18:07:15 +0200

    COmmmit prova 2

--- Homepage.js
+++ Homepage.js
@@ -1,19 +1,5 @@
-fetch("https://striveschool-api.herokuapp.com/api/deezer/",{
-    headers: {
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDYyM2M3ODc3NTgzYzAwMTRkMmNjYmMiLCJpYXQiOjE2ODQxNTk2MDksImV4cCI6MTY4NTM2OTIwOX0.RK6RE8424MDCQbvs8u0gvKiPo4MrR6-ww1HYYA6TexE"
-    }})
-
-.then(res => {
-    if (res.ok) {
-        console.log(res)
-        return res.json()
-    } else {
-        throw new Error("errore fetch")
-    }
+fetch("https://striveschool-api.herokuapp.com/api/deezer/album")
+.then(res=>{
+    if(res.ok) return res.json();
 })
-.then(data => {
-    console.log(data)
-})
-.catch(err => {
-    console.log(err)
-})
+.then(data=> console.log(data))
