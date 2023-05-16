f1d9c3022eca8ec645894ae4589848ef62f17147
daviaprea <apredav@gmail.com>
2023-05-16 19:20:24 +0200

    Dinamicizzazione pagina album

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,18 +1,26 @@
-fetch("https://striveschool-api.herokuapp.com/api/deezer/",{
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=theweeknd",{
     headers: {
     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDYyM2M3ODc3NTgzYzAwMTRkMmNjYmMiLCJpYXQiOjE2ODQxNTk2MDksImV4cCI6MTY4NTM2OTIwOX0.RK6RE8424MDCQbvs8u0gvKiPo4MrR6-ww1HYYA6TexE"
     }})
 
 .then(res => {
     if (res.ok) {
-        console.log(res)
         return res.json()
     } else {
         throw new Error("errore fetch")
     }
 })
-.then(data => {
-    console.log(data)
+.then(fetchObj => {
+    console.log(fetchObj)
+    let albumCards=document.querySelectorAll("#albumRow .colCentralPlaylistCard");
+    albumCards.forEach((card, i)=>{
+        card.querySelector("img").src=fetchObj.data[i].album.cover_medium;
+        card.querySelector("h5 a").innerHTML=fetchObj.data[i].album.title;
+        card.querySelector("h5 a").href=`album.html?albumId=${fetchObj.data[i].album.id}`;
+
+        card.querySelector("p a").innerHTML=fetchObj.data[i].artist.name;
+        card.querySelector("p a").href=`artist.html?artistId=${fetchObj.data[i].artist.id}`;
+    });
 })
 .catch(err => {
     console.log(err)
