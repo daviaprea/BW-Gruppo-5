b84970c68ccb72da7879c669d2ce14bbacb34574
daviaprea <apredav@gmail.com>
2023-05-17 15:57:56 +0200

    Risoluzione bug artist page

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -14,19 +14,20 @@ fetch(endPoint + artist + '/' + 'top?limit=50')
 .then(artist => {
     console.log(artist);
 
-    let popularSongs = document.querySelectorAll('#popular-songs div');
-    popularSongs.forEach((list, i) => {
-        let bgImage = document.getElementById('artist-information') 
-        bgImage.style.background = `linear-gradient(0deg, rgba(0,0,0,.8), rgba(0,0,0,.3)), url(${artist.data[i].contributors[0].picture_xl})`;
+    let bgImage = document.getElementById('artist-information') 
+        bgImage.style.background = `linear-gradient(0deg, rgba(0,0,0,.8), rgba(0,0,0,.3)), url(${artist.data[0].contributors[0].picture_xl})`;
         bgImage.style.backgroundSize = 'cover';
         bgImage.style.backgroundPosition = 'center 20%';
         bgImage.style.position = 'relative';
 
-        let nameArtist = document.getElementById('name-artist');
-        nameArtist.innerHTML = `${artist.data[i].contributors[0].name}`;
+    let nameArtist = document.getElementById('name-artist');
+    nameArtist.innerHTML = `${artist.data[0].contributors[0].name}`;
 
-        let listener = document.getElementById('listeners');
-        listener.innerHTML = `${artist.data[i].contributors[0].nb_fan}`;
+    let popularSongs = document.querySelectorAll('#popular-songs>div');
+    popularSongs.forEach((list, i) => { 
+
+        /*let listener = document.getElementById('listeners');
+        listener.innerHTML = `${artist.data[i].contributors[0].nb_fan}`;*/
 
         list.querySelector('img').src = artist.data[i].album.cover_small;
         list.querySelector('img').style.width = '30px'
